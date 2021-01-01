b33364a426e96da84311cf3b2a2c0a95a9d51d0d
ruslan <ruslan0-0abdullaev@mail.ru>
2021-01-02 00:24:54 +0300

    small changes

--- homework1/2.py
+++ homework1/2.py
@@ -1,9 +1,11 @@
 # написать функцию, которая на вход принимает список из целых чисел, и возвращает только чётные/нечётные/простые числа
 # (выбор производится передачей дополнительного аргумента)
+
 list = []
 b = 1
 question = int(input('если вы хотите найти чётные, нечётные, или же простые числа, введите 1, 2 или 3 соответсвенно'))
 print('вводите целые числа, если хотите закончить ввод, введите 0 :')
+
 while b != 0:
     b = int(input(''))
     list.append(b)
@@ -11,10 +13,12 @@ def even(mini_digit):
     for i in range (1, len(list)):
         if i % 2 == 0:
             print(i)
+
 def odd(mini_digit):
     for i in range (1, len(list)):
         if i % 2 == 1:
             print(i)
+
 def simple(number):
     n = number
     counter = 0
@@ -26,8 +30,10 @@ def simple(number):
 
 if question == 1:
     even(len(list))
+
 if question == 2:
     odd(len(list))
+
 if question == 3:
     for j in list:
         simple(j)
