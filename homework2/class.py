dd5afdbfada7a52d09644d2ab80f67dd4809ca75
Ruslan365 <53355002+Ruslan365@users.noreply.github.com>
2021-02-11 02:40:04 +0300

    Update class.py

--- homework2/class.py
+++ homework2/class.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod, abstractproperty , ABCMeta
 
 class Vehicle:
     type_name = None
@@ -17,7 +17,6 @@ class Plane(Vehicle):
     def __init__ (self, price, weight, lift_capaciti, max_hight):
         super().__init__(price, weight, lift_capaciti)
         self.max_hight = max_hight
-
 class Car(Vehicle):
     type_name = "автомобиль"
     def __init__(self,price, weight, lift_capaciti,wheel_quantity):
@@ -45,11 +44,11 @@ class Military_plane(Plane):
     type_name = "ави"
     pass
 
-demo1 = Military_plane(0,1,2,3)
-print (demo1.price, demo1.weight, demo1.lift_capaciti, demo1.max_hight)
-demo2 = Light_car(0,1,2,3,4)
-print (demo2.price, demo2.weight, demo2.lift_capaciti, demo2.wheel_quantity,demo2.speed )
-demo3 = Car(0,1,2,3)
-print (demo3.price, demo3.weight, demo3.lift_capaciti, demo3.wheel_quantity)
-
-Vehicle.total_objects()
+if __name__ == "__main__":
+    demo1 = Military_plane(0,1,2,3)
+    print (demo1.price, demo1.weight, demo1.lift_capaciti, demo1.max_hight)
+    demo2 = Light_car(0,1,2,3,4)
+    print (demo2.price, demo2.weight, demo2.lift_capaciti, demo2.wheel_quantity,demo2.speed )
+    demo3 = Car(0,1,2,3)
+    print (demo3.price, demo3.weight, demo3.lift_capaciti, demo3.wheel_quantity)
+    Vehicle.total_objects()
